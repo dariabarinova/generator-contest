26f36ae1340666b3d6dee03fb681a94248ac5dd6
Igor Sachivka <juicy.igor@gmail.com>
2018-07-29 20:48:17 +0300

    Added <Text /> component, fonts

--- src/ui/Text/Text.js
+++ src/ui/Text/Text.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import './text.css';
 
-const Text = ({ children }) => (
-  <span className="text">
+const Text = ({
+  children,
+  bold,
+  heavy,
+}) => (
+  <span
+    className={cx('text', {
+      'text-heavy': heavy,
+      'text-bold': bold,
+    })}
+  >
     {children}
   </span>
 );
 
 Text.propTypes = {
   children: PropTypes.string.isRequired,
+  bold: PropTypes.bool,
+  heavy: PropTypes.bool,
 };
+
+Text.defaultProps = {
+  bold: false,
+  heavy: false,
+}
 
 export default Text;
